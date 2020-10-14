f8fecf51d31db111ff0c6cdec2b1d14b8596c23a
“dineshega” <dinesh15.ega@gmail.com>
2020-10-14 15:57:32 +0530

    Added: create regex pattern pincode

--- pincode.sh
+++ pincode.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "welcome to Pincode problem"
+
+read -p "enter pincode: " pincode
+
+pat1='^[0-9]{6}$'
+
+if [[ $pincode =~ $pat1 ]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
