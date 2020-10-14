01f2f73b867baebbf0c8996a101e06d89933b0a0
“dineshega” <dinesh15.ega@gmail.com>
2020-10-14 17:04:40 +0530

    Added: char part

--- email.sh
+++ email.sh
@@ -5,7 +5,7 @@ echo "Welcome to email pattern problem"
 shopt -s extglob
 
 read -p "enter Email: " email
-pat1='^[a-zA-z0-9]+@[a-zA-Z0-9]+.[a-z]{2,4}$'
+pat1='^[a-zA-z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,4}$'
 
 if [[ $email =~ $pat1 ]]
 then
