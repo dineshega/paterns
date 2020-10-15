f98c19b0b1366c20bce13e03fe8e13cf36e18152
“dineshega” <dinesh15.ega@gmail.com>
2020-10-15 13:02:47 +0530

    Added: mobile added

--- UserReg.sh
+++ UserReg.sh
@@ -2,12 +2,14 @@
 
 echo "Welcome to User Registration Problem"
 
-read -p "enter Email: " email
-pat1='^[a-zA-z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,4}([.][a-z]{2})*$'
+read -p "enter mobile number: " mb
+pat1='^((\+){1}91){1}[ ]?[0-9]{10}$'
+# +91 9876543210
+# +919876543210
 
-if [[ $email =~ $pat1 ]]
+if [[ $mb =~ $pat1 ]]
 then
-	echo "email registered"
+	echo "mobile registered"
 else
-	echo "email not registered"
+	echo "mobile not registered"
 fi
