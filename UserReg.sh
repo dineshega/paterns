a7918c4343dc84769b98c87b6b03b38ec271353b
“dineshega” <dinesh15.ega@gmail.com>
2020-10-15 13:09:58 +0530

    Added: password rule 2

--- UserReg.sh
+++ UserReg.sh
@@ -5,11 +5,11 @@ echo "Welcome to User Registration Problem"
 shopt -s extglob
 
 read -p "Enter password: " pass
-patpass='^[A-za-z]{8,}$'
+patpass='^(?=.*[a-z])(?=.*[A-Z]).{8,}$'
 
 if [[ $pass =~ $patpass ]]
 then
 	echo "password set"
 else
 	echo "passwors not set"
-fi 
+fi
