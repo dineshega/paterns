4566399061d05fcbede2df5c215af810b62033fe
“dineshega” <dinesh15.ega@gmail.com>
2020-10-15 12:54:44 +0530

    Added: First Name

--- UserReg.sh
+++ UserReg.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to User Registration Problem"
+
+read -p "Enter First Name: " Fname
+patfname='^[A-Z]{3,}$'
+
+if [[ $Fname =~ $patfname ]]
+then
+	echo "name registered"
+else
+	echo "name not registered"
+fi
